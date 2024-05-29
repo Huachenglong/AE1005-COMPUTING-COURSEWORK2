e3d1b96d603fd32079a29557e70a39136b425ba4
Huachenglong <ssych3@nottingham.edu.cn>
2024-05-29 22:59:44 +0800

    Add files via upload

---
+++ doc_tem_monitor.m
@@ -0,0 +1,3 @@
+function doc_tem_monitor
+sprintf('Firstly, the LED lamp is inserted into the bread board, and the state of the LED is determined by the temperature. After the measurement of temperature and time is completed, the temperature control function is realized to help the control of the state of the LED lamp.')
+end
